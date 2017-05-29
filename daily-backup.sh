3b025f6af7c39ed25b78419a3f805945d382f0cd
Guillaume Aubert <aubertg@cpan.org>
2017-05-28 21:32:08 -0700

    Add script to handle backups.

---
+++ daily-backup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -euf -o pipefail
+
+# Define a horizontal line.
+HLINE=$(printf '%*s\n' 80 '' | sed -e 's/ /─/g')
+
+# Record start datetime.
+date
+echo "$HLINE"
+
+# Update go-git-backup to the latest version of the code.
+echo "Updating go-git-backup..."
+cd $GOPKGDIR
+git pull
+echo "$HLINE"
+
+# Back up git repositories.
+go run $GOPKGDIR/gitbackup.go -config /etc/gitbackuprc.yml
+echo "$HLINE"
